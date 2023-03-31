dbad62cbd2f4f7ec8fc6cc0f39e4c44c0371da7a
Tinawang0809 <112570845+Tinawang0809@users.noreply.github.com>
2023-03-31 11:39:06 -0700

    Update Createproject.py

--- Createproject.py
+++ Createproject.py
@@ -10,113 +10,126 @@
 # #decypher the encrypted text
 # decrypted_text = cipher.decrypt(encrypted_text)
 # print(decrypted_text)
-import os, random, datetime
+import os
 
 class Persistent:
-    file = "";
-    cache = []
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self) -> None:
+       self._file_name="my.db"
+       self._data={}
+       if os.path.exists(self._file_name):
+        self._load_data()
+       else:
+        with open(self._file_name,'w+') as f:
+            pass
+#independent user files that records each user's own account and password data
 
-#get password and account
+    def _save_data(self):
+        with open(self._file_name, 'w+') as f:
+            for key,value in self._data.items():
+                f.write(key+"@#$@#$"+value+"\n")
+        print("Save the dict into file successfully .............")
+# encryption of the user's password in the file
+
+    def _load_data(self):
+        self._data={};
+        with open(self._file_name, 'r+') as f:
+            for line in f.readlines():
+                line_list=line.split("@#$@#$")
+                self._data[line_list[0]]=line_list[1]
+
+    # def __del__(self):
+    #     self._save_data()
+
+    def get(self, key):
+        return self._data.get(key)
+
+    def set(self, key, value):
+        self._data[key]=value
+
+    def flush(self):
+        self._save_data()
+
+    def existAccount(self, account):
+        return self._data.get(account) != None
+
     def getAccount(self, account):
-            self.file = open(self.filename, "r")
-            lines = self.file.readlines()
-            # Strips the newline character
-            for line in lines:
-                print(line)
-                line_list = line.split("\t")
-                print(line_list)
-     #Based on the file entered, returning to current account's information(password)
+        return self._data.get(account)
 
-     def updateAccount(self, account, password):
-        pass
-        # Update the current file's account information (Password)
+    def updateAccount(self, account, password):
+        self._data[account]=password
+        self.flush()
 
     def createAccount(self, account, password):
-        pass
-        # Adding the new account/ account's password information into the user file
+        self._data[account]=password
+        self.flush()
 
     def deleteAccount(self, account):
-        pass
-        # delete the targeted account information entered by the user in the user file
+        del self._data[account]
+        self.flush()
 
     def listAccount(self):
-        pass
-        # List and print all the account information in the user file
+        return list(self._data.keys())
 
-
-
-# main menu
-my_perststent = Persistent("db")
+my_perststent = Persistent()
 while True:
-    print("______________  My Password Assistant  ______________")
-    print("______________  1) Create account      ______________")
-    print("______________  2) Service page        ______________")
-    print("______________  3) exit  ***********************")
+    print("-----------------------    My Password Assistant  -----------------------")
+    print("-----------------------  1) Access password       -----------------------")
+    print("-----------------------  2) Update your password  -----------------------")
+    print("-----------------------  3) Delete your password  -----------------------")
+    print("-----------------------  4) Print account list    -----------------------")
+    print("-----------------------  0) Exit                  -----------------------")
     cmd = input("Please choose your option: ")
-    if cmd == 3 or cmd == '3':
-        print("Thanks for you use.")
-        exit()
-
-
-
-
-# service menu
-    print("####################################")
-    print("#         Service Page             #")
-    print("#       1 Add Account              #")
-    print("#       2 Update Account           #")
-    print("#       3 Delete Account           #")
-    print("#       4 Display list             #")
-    print("#       5 Exit                     #")
-    print("########################################")
-    choice = input("Enter your choice ")
-        os.system('clear')
-    if '1' in choice:
-        print(" enter a random letter to start guessing the word! If the letter is within the word, it will show you !!!")
-        input(" Press return to go back to main menu" )
-        Menu()
-    elif '6' in choice:
-        print ("good Bye")
-        exit()
-    elif '2' in choice or '3' in choice or '4' in choice:
-        selectWord(choice)
-    elif '5' in choice:
-        print_scoreboard()
-        input(" Press return to go back to main menu" )
-        Menu()
-
-    else:
-        print("PLease enter a valid option")
-        Menu()
     if cmd == 0 or cmd == '0':
-        print("Thanks for you use.")
+        print("Thanks for using.")
         exit()
     elif cmd == 1 or cmd == '1':
-        account = input("Please enter your account that your need the password: ")
+        account = input("Please enter your account: ")
         #TODO get the password from file
-        my_perststent.getAccount(account);
+        if my_perststent.existAccount(account):
+            print("You password for this account is: " + str(my_perststent.getAccount(account)));
+        else:
+            print("The accout your input is not exist in this system")
+
     elif cmd == 2 or cmd == '2':
-        account = input("Please enter your account that your need the password: ")
-        #TODO 1. Check whether current account exists in the file
-        #TODO 3: Can we set the input to '*' while input password ?
-        password = input("Please enter your new password for this account.")
-        confirmed_password = input("Please enter your new password for this account again.")
-        #TODO 2. Create/Update password into current account
-        # validate your input !!!
-        # check option create new or update ???
-        my_perststent.updateAccount(account, password)
-        my_perststent.createAccount(account, password)
+        account = input("Please enter your account: ")
+        if my_perststent.existAccount(account):
+            print("Would you like to update the password? ")
+            password="0"
+            confirmed_password="1"
+            while password != confirmed_password:
+                password = maskpass.askpass(prompt="Password:",mask="*")
+                confirmed_password = maskpass.askpass(prompt="Confirm Password:",mask="*")
+                # input("Please enter your new password for this account.")
+                # confirmed_password = input("Please enter your new password for this account again.")
+                if password != confirmed_password:
+                    print("The entered passworded does not match, please try again")
+            my_perststent.updateAccount(account, password)
+            print("Password updated successfully...")
+        else:
+            print("Create a new account: ")
+            password="0"
+            confirmed_password="1"
+            while password != confirmed_password:
+                password = maskpass.askpass(prompt="Password:",mask="*")
+                confirmed_password = maskpass.askpass(prompt="Confirm Password:",mask="*")
+                # password = input("Please enter your new password for this account.")
+                # confirmed_password = input("Please enter your new password for this account again.")
+                if password != confirmed_password:
+                    print("The password your input is not match in twice, please input again")
+            my_perststent.createAccount(account, password)
+            print("Password created successfully..")
+
     elif cmd == 3 or cmd == '3':
-        account = input("Please enter your account that your need to delete password: ")
-        # TODO 1. Check whether current account exists in the file
-        # TODO 2. Delete the account with the password
-        my_perststent.deleteAccount(account)
+        account = input("Enter your account: ")
+        if my_perststent.existAccount(account):
+            my_perststent.deleteAccount(account)
+            print("Your account deleted successfully.")
+        else:
+            print("Your input account is not exist in the system and no need to delete.")
     elif cmd == 4 or cmd == '4':
-        pass
-        # TODO 1. print("List all accounts: ")
-        # TODO 2. print("Search all accounts: ")
-        my_perststent.listAccount()
+        print("List all accounts as below: ")
+        account_list = my_perststent.listAccount()
+        for account in account_list:
+            print(account)
     else:
         print("Your input is invalid, please choose the correct option !!!")
